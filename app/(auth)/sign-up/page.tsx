40709c891fcdb1d07dec3555746923e9502fb9ce
gitdeepaks <deepaksankhyan92@outlook.com>
2024-04-24 23:39:01 +0530

    added dashboardpage partially with otp

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -46,7 +46,7 @@ export default function SignUpForm() {
     const checkUsernameUnique = async () => {
       if (username) {
         setIsCheckingUsername(true);
-        setUsernameMessage(""); // Reset message
+        setUsernameMessage("");
         try {
           const response = await axios.get<ApiResponse>(
             `/api/check-username-unique?username=${username}`
@@ -102,7 +102,7 @@ export default function SignUpForm() {
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
         <div className="text-center">
           <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-            Join True Feedback
+            Join Random Feedback
           </h1>
           <p className="mb-4">Sign up to start your anonymous adventure</p>
         </div>
